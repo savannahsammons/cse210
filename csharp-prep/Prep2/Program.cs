c159a137e013b4064e4e48ad0d15705e290a8758
savannahsammons <sam20003@byui.edu>
2024-11-01 12:21:00 -0600

    Did Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your number grade? ");
+        string number_grade = Console.ReadLine();
+
+        int grade = int.Parse(number_grade);
+        string letter = " ";
+        if (grade >= 90) {
+            letter = "A";
+        }
+        else if (grade >= 80) {
+            letter = "B";
+        }
+        else if (grade >= 70) {
+            letter = "C";
+        }
+        else if (grade >= 60) {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (grade >= 70) {
+            Console.WriteLine("Congratulations! You Passed!");
+        }
+        else {
+            Console.WriteLine("Looks you didn't pass... better luck next time!");
+        }
     }
 }
