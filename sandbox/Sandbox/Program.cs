4ba8fd19c5d1d248f900ae4b33034c806bcaa268
savannahsammons <sam20003@byui.edu>
2024-11-04 09:56:56 -0700

    class stuff Nov 4

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,13 +6,47 @@ class Program
 {
     static int AddNumbers(int n1, int n2)
     {
-        return n1 + n2;
+        int total = n1+n2;
+        // int total2 = n1*n2;
+        return total;
     }
     static void Main(string[] args)
     {
+
+        int total = AddNumbers(20, 30);
+        Console.WriteLine(total);
+
         for(int i = 0; i < 10; i++)
         {
             Console.WriteLine(i);
+        }
+
+        int age = -1;
+
+        while(age < 0 || age > 120)
+        {
+            Console.WriteLine("Please enter your age (0-124): ");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your age is {age}");
+        }
+
+        int age1;
+        do
+        {
+            Console.WriteLine("Please enter your age (0-124): ");
+            age1 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your age is {age}");
+        } while(age1 < 0 || age1 > 120);
+
+        List <string> myColors = new List<string>();
+
+        myColors.Add("Red");
+        myColors.Add("Green");
+        myColors.Add("Blue");
+
+        foreach(string color in myColors)
+        {
+            Console.WriteLine(color);
         }
     }
 }
