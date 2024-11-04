fb165bf5bb03a381fdd2688ebda4d71b27f28213
savannahsammons <sam20003@byui.edu>
2024-11-04 09:29:18 -0700

    save class example

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,10 +1,18 @@
 using System;
+using System.ComponentModel;
 
 
 class Program
 {
+    static int AddNumbers(int n1, int n2)
+    {
+        return n1 + n2;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
