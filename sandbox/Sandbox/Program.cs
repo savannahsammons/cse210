cd8d624b25ad2681df91d9af0e7d8e497e80f896
savannahsammons <sam20003@byui.edu>
2024-11-05 09:32:46 -0700

    added ++y and x++ difference

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -12,41 +12,47 @@ class Program
     }
     static void Main(string[] args)
     {
+        int x = 10;
+        int y = x++;
+        Console.WriteLine($"{y}, {x}");
+        
+        int z = ++y;
+        Console.WriteLine($"{z}, {y}");
 
-        int total = AddNumbers(20, 30);
-        Console.WriteLine(total);
+        // int total = AddNumbers(20, 30);
+        // Console.WriteLine(total);
 
-        for(int i = 0; i < 10; i++)
-        {
-            Console.WriteLine(i);
-        }
+        // for(int i = 0; i < 10; i++)
+        // {
+        //     Console.WriteLine(i);
+        // }
 
-        int age = -1;
+        // int age = -1;
 
-        while(age < 0 || age > 120)
-        {
-            Console.WriteLine("Please enter your age (0-124): ");
-            age = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Your age is {age}");
-        }
+        // while(age < 0 || age > 120)
+        // {
+        //     Console.WriteLine("Please enter your age (0-124): ");
+        //     age = int.Parse(Console.ReadLine());
+        //     Console.WriteLine($"Your age is {age}");
+        // }
 
-        int age1;
-        do
-        {
-            Console.WriteLine("Please enter your age (0-124): ");
-            age1 = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Your age is {age}");
-        } while(age1 < 0 || age1 > 120);
+        // int age1;
+        // do
+        // {
+        //     Console.WriteLine("Please enter your age (0-124): ");
+        //     age1 = int.Parse(Console.ReadLine());
+        //     Console.WriteLine($"Your age is {age}");
+        // } while(age1 < 0 || age1 > 120);
 
-        List <string> myColors = new List<string>();
+        // List <string> myColors = new List<string>();
 
-        myColors.Add("Red");
-        myColors.Add("Green");
-        myColors.Add("Blue");
+        // myColors.Add("Red");
+        // myColors.Add("Green");
+        // myColors.Add("Blue");
 
-        foreach(string color in myColors)
-        {
-            Console.WriteLine(color);
-        }
+        // foreach(string color in myColors)
+        // {
+        //     Console.WriteLine(color);
+        // }
     }
 }
