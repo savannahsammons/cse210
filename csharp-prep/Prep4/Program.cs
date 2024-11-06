0e82a3efb79a99633ade2c460a71ffc53d825074
savannahsammons <sam20003@byui.edu>
2024-11-06 10:23:10 -0700

    Really finished prep4 and fixed math issues

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,39 +7,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-        Console.Write("Enter number: ");
-        int check = 0;
         List <int> numbers = new List<int>();
 
         int sum = 0;
-        int average = 0;
+        float average = 0;
         int largest = 0;
 
-        while (check != 0)
+        while (true)
         {
+            Console.Write("Enter number: ");
             int newNumber = int.Parse(Console.ReadLine());
-            numbers.Add(newNumber);
             if (newNumber == 0)
             {
-                check = 0;
+                break;
             }
-
+            numbers.Add(newNumber);
         }
 
         foreach (int num in numbers)
         {
             sum += num;
-        }
-        foreach (int num in numbers)
-        {
-            average = sum / numbers.Count;
-        }
-        foreach (int num in numbers)
-        {
             if (num > largest)
             {
                 largest = num;
             }
+        }
+        
+        if (numbers.Count > 0)
+        {
+            average = (float)sum / numbers.Count;
         }
 
         Console.WriteLine($"The sum is: {sum}");
