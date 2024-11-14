1aeddc7cb306ce5a961032713c3827ba82cf542f
savannahsammons <sam20003@byui.edu>
2024-11-14 09:36:12 -0700

    made ToString() and ReadFromFile()

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -14,6 +14,28 @@ class Program
         }
 
     }
+    public override string ToString()
+    {
+        string outPutString = "";
+        outputString = $"{date}#{_entryQuestion}#{_journalEntry}";
+        return outputString();
+    }
+    public void ReadFromFile(string filename)
+    {
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("#");
+            
+            string date = parts[0];
+            string question = parts[1];
+            string entryText = parts[2];
+
+            JournalEntry entry = new JournalEntry(date, question, entryText);
+            this.AddEntry(entry);
+        }
+    }
+
     static void Main(string[] args)
     {
         
