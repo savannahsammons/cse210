7dfe479d377953f7a8ee2f35ec9f0ec9dab6c3a9
savannahsammons <sam20003@byui.edu>
2024-11-14 17:31:05 -0700

    created and finished prompts class with journal entry also updated to call it to give a new prompt

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -4,19 +4,19 @@ using System.ComponentModel.DataAnnotations;
 class JournalEntry
 {
     private List <string> entries = new List<string>();
+    private Prompts prompts;
 
     public JournalEntry()
     {
-        // idk
+        prompts = new Prompts();
     }
     public JournalEntry(int date, string prompt, string response)
     {
         Console.WriteLine($"{date}: Prompt- {prompt}");
         Console.WriteLine(response);
     }
-
-    public void DisplayAllEntries()
+    public void CreateEntryWithPrompt()
     {
+        Console.WriteLine(prompts.NextQuestion());
     }
-
 }
---
+++ prove/Develop02/Prompts.cs
@@ -0,0 +1,14 @@
+
+class Prompts
+{
+    private List<string> _questions = ["Who was the most interesting person I interacted with today?", "What was the best part of my day?", "How did I see the hand of the Lord in my life today?", "What was the strongest emotion I felt today?", "If I had one thing I could do over today, what would it be?"];
+
+    public string NextQuestion()
+    {
+        Random rnd = new Random();
+        int randomNum = rnd.Next(0, _questions.Count());
+        // Console.WriteLine(_questions[randomNum]);
+
+        return _questions[randomNum];
+    }
+}
