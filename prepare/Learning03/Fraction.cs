b6e9a7139715e7abf861c404808d3f98b9f2751f
savannahsammons <sam20003@byui.edu>
2024-11-20 09:12:36 -0700

    worked on fraction.cs and program.cs for develop and learning 03

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -2,7 +2,47 @@
 class Fraction
 {
     private int _top;
-    private int bottom;
+    private int _bottom;
 
-    
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void GetTop()
+    {
+        // idk
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public void GetBottom()
+    {
+        // idk
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public void GetFractionString()
+    {
+        // idk should return string not void
+    }
+    public void GetDecimalValue()
+    {
+        // idk should return double not void
+    }
 }
