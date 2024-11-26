9f312ffdc47b37b8b3fd12d9d96c07349e56c176
savannahsammons <sam20003@byui.edu>
2024-11-26 09:36:20 -0700

    added this will be the menu filler

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,5 +1,12 @@
 
 class Activity
 {
-    
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public void DisplayMenu()
+    {
+        Console.WriteLine("This will be the menu...");
+    }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Activity act = new Activity();
+
+        act.DisplayMenu();
     }
 }
