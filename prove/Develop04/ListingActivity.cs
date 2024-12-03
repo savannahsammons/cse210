34554bba2c11819be8ed0587f97345699f2db2da
savannahsammons <sam20003@byui.edu>
2024-12-02 21:48:17 -0700

    finished listing activity without testing yet though

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -1,17 +1,48 @@
 
 class ListingActivity : Activity
 {
-    public ListingActivity(): base("Listing", "Description of listing..."){}
+    private List<string> prompts = new List<string>{"-- When have you felt the Holy Ghost this month? --", "-- When have you felt the Savior's love this week? --", "-- When have you served others today? --"};
+    private List<string> responses = new List<string>{};
+    public ListingActivity(): base("Listing", "This activity will help you reflect on the good things in your life by having you list as many things you can in a certain area."){}
 
     public void SetDuration(int duration)
     {
         _duration = duration; // Set the duration dynamically
     }
 
+    private string GetRandomPrompt()
+    {
+        string prompt = prompts[0];
+        return prompt;
+    }
+
     public void DisplayListing()
     {
-        Console.WriteLine("List as many responses as you can to the following prompt...");
+        Console.WriteLine("List as many responses as you can to the following prompt:");
+        string prompt = GetRandomPrompt();
+        Console.WriteLine(prompt);
 
+        Console.WriteLine("You may begin in...");
+        Timer();
+
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(_duration);
+
+        while (true)
+        {
+            DateTime currentTime = DateTime.Now;
+            string response = Console.ReadLine();
+            responses.Add(response);
+            
+            if (currentTime > futureTime)
+            {
+                break;
+            }
+        }
+
+        int listLength = responses.Count();
+        Console.WriteLine($"You Listed {listLength} items!");
+        Console.WriteLine("");
     }
     
 }
