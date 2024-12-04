054b8d7fa0a77529f1fbe78622c11b7837ab8e7a
savannahsammons <sam20003@byui.edu>
2024-12-04 08:20:05 -0700

    made square and added to main

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello Learning05 World!");
+        Square square1 = new Square("blue", 5);
+
+        string color = square1.GetColor();
+        double area = square1.GetArea();
+        Console.WriteLine(color);
+        Console.WriteLine(area);
     }
 }
